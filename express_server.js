993201ec1ceb447c7f5b0723ffe8c6dae828e9e0
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-11-10 14:14:51 +0000

    including nodemon to automatically restart the server when monitoring any changes

--- express_server.js
+++ express_server.js
@@ -38,11 +38,13 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString(6); //generate a unique 6 digits string for shortURL
   urlDatabase[shortURL] = req.body.longURL; //assign the shortURL to longURL to save the pair to urlDB
   res.redirect("/urls/" + shortURL);
 });
+//after the browser receives a redirection res, it GET req to the url in the res.
 //The order of route definitions matters! The GET /urls/new route needs to be defined before the GET /urls/:id route. Routes defined earlier will take precedence, so if we place this route after the /urls/:id definition, any calls to /urls/new will be handled by app.get("/urls/:id", ...) because Express will think that new is a route parameter. A good rule of thumb to follow is that routes should be ordered from most specific to least specific.
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
